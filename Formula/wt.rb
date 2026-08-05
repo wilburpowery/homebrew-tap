e47d49c94027cb8e4288945495311a9ddaa7f6dc
Wilbur Powery <wilburpowery@gmail.com>
2026-08-05 14:28:42 -0600

    wt 0.2.1

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -1,8 +1,8 @@
 class Wt < Formula
   desc "Instant Laravel worktrees with Herd: branch, cloned DB, TLS site in one command"
   homepage "https://github.com/wilburpowery/wt"
-  url "https://github.com/wilburpowery/wt/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "2ac39fb8661dac6c42700e8a30b6a5808187bba3d4ffda663e045f9ac04e8dac"
+  url "https://github.com/wilburpowery/wt/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "ef4889fcd76b414499a279dcfab6dc7d2fe395dd5a199bf2122680bd115cfde2"
   license "MIT"
   head "https://github.com/wilburpowery/wt.git", branch: "main"
 
